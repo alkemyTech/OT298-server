2543e4c7707684dbdef18942f8b289634248593c
fercuche <fer2.maciel@gmail.com>
2022-09-16 13:33:04 -0300

    Testimonial model and repository added

---
+++ src/main/java/com/alkemy/ong/model/Testimonial.java
@@ -0,0 +1,39 @@
+package com.alkemy.ong.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+import org.springframework.lang.Nullable;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.sql.Timestamp;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "testimonials")
+@SQLDelete(sql = "UPDATE testimonials SET deleted = true WHERE id = ?")
+@Where(clause = "deleted = false")
+public class Testimonial {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "Name cannot be null")
+    private String name;
+
+    @Nullable
+    private String image;
+
+    @Nullable
+    private String content;
+
+    private Timestamp timestamps;
+
+    private Boolean deleted = Boolean.FALSE;
+}
