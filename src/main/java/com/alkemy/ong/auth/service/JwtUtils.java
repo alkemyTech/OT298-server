edacbf4181975faa25fe330f1c2c10ea162e99f5
laudebor <laudebor@gmail.com>
2022-09-16 19:24:23 -0300

    generateToken and createToken methods

--- src/main/java/com/alkemy/ong/auth/service/JwtUtils.java
+++ src/main/java/com/alkemy/ong/auth/service/JwtUtils.java
@@ -1,8 +1,27 @@
 package com.alkemy.ong.auth.service;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class JwtUtils {
+
+    private String SECRET_KEY = "secret";
+    public String generateToken(UserDetails userDetails){
+        Map<String, Object> claims = new HashMap<>();
+        return createToken(claims, userDetails.getUsername());
+    }
+
+    private String createToken(Map<String, Object> claims, String subject){
+        return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis()+1000*60*60*10))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY).compact();
+    }
 
 }
