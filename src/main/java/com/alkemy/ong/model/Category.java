4e0b31ed4189277b779d748c80fb9afd827eb3b5
Alejandro <alj70111@gmail.com>
2022-09-18 21:09:24 -0300

    updated category entity in a new branch

---
+++ src/main/java/com/alkemy/ong/model/Category.java
@@ -0,0 +1,40 @@
+package com.alkemy.ong.model;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "categories")
+@Getter
+@Setter
+@SQLDelete(sql = "UPDATE categories SET deleted = true WHERE id=?")
+@Where(clause = "deleted=false")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "User name cannot not be empty")
+    private String name;
+
+    private String description;
+    private String image;
+
+    @Column(name = "creation_date")
+    @CreationTimestamp
+    private Timestamp creationDate;
+
+    @Column(name = "update_date")
+    @UpdateTimestamp
+    private Timestamp updateDate;
+
+    private boolean deleted = Boolean.FALSE;
+
+}
