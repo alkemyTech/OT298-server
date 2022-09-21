f478ca9ae8013a58b204051e230355fd73e53fc0
joeldevel <christianjoel.developer@gmail.com>
2022-09-21 17:27:16 -0300

    replaces constructor with lombok annotations

--- src/main/java/com/alkemy/ong/dto/OrganizationBasicDTO.java
+++ src/main/java/com/alkemy/ong/dto/OrganizationBasicDTO.java
@@ -1,21 +1,19 @@
 package com.alkemy.ong.dto;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrganizationBasicDTO {
     private String name;
     private Integer phone;
     private String address;
     private String image;
 
-    public OrganizationBasicDTO(String name, Integer phone, String address, String image) {
-        this.name = name;
-        this.phone = phone;
-        this.address = address;
-        this.image = image;
-    }
 }
