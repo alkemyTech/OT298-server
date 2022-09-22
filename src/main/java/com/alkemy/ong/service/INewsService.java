a7c2e83b4da9049ecf63a09254c2fa8092d42581
montexbjeliseo <secondmtx@gmail.com>
2022-09-22 15:51:42 -0300

    News Post Endpoint Added

---
+++ src/main/java/com/alkemy/ong/service/INewsService.java
@@ -0,0 +1,16 @@
+package com.alkemy.ong.service;
+
+import com.alkemy.ong.dto.NewsDto;
+import com.alkemy.ong.model.News;
+
+import java.util.Optional;
+import java.util.List;
+
+public interface INewsService {
+    NewsDto save(NewsDto dto);
+    NewsDto update(NewsDto dto); 
+    void delete(Long id);
+    boolean existById(Long id);
+    Optional<News> findById(Long id);
+    Optional<List> findAll(int index);
+}
