2310f4ecc74dc436e0816b49331c5035a0efdac0
andyholes <andyholesdev@gmail.com>
2022-09-23 03:35:51 -0300

    Revert "Modify customException name"
    This reverts commit 426360bb09ad0cf9ae7183a4a30e2dba39868e08.

--- src/main/java/com/alkemy/ong/security/service/impl/UserServiceImpl.java
+++ src/main/java/com/alkemy/ong/security/service/impl/UserServiceImpl.java
@@ -36,7 +36,7 @@ public class UserServiceImpl implements IUserService, UserDetailsService {
     public UserGetDto registerUser(UserPostDto dto) {
 
         if (userRepository.existsByEmail(dto.getEmail())) {
-            throw new AlreadyExistsException(
+            throw new CustomException(
                     "There is an account with that email adress:" + dto.getEmail(), HttpStatus.BAD_REQUEST);
         }
 
