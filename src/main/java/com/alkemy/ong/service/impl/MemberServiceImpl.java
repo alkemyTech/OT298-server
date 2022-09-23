637fcb48d4e9165b923945b73abd568fc199d2f5
laudebor <laudebor@gmail.com>
2022-09-23 14:41:38 -0300

    Handle exception from save method

--- src/main/java/com/alkemy/ong/service/impl/MemberServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/impl/MemberServiceImpl.java
@@ -1,11 +1,13 @@
 package com.alkemy.ong.service.impl;
 
 import com.alkemy.ong.dto.MemberDTO;
+import com.alkemy.ong.exception.CustomException;
 import com.alkemy.ong.mapper.MemberMapper;
 import com.alkemy.ong.model.Member;
 import com.alkemy.ong.repository.MemberRepository;
 import com.alkemy.ong.service.IMemberService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -21,8 +23,12 @@ public class MemberServiceImpl implements IMemberService {
 
     @Transactional
     public MemberDTO save(MemberDTO dto){
-        Member member = memberMapper.memberDTOToMember(dto);
-        Member savedMember = memberRepository.save(member);
-        return memberMapper.memberToMemberDTO(savedMember);
+        try {
+            Member member = memberMapper.memberDTOToMember(dto);
+            Member savedMember = memberRepository.save(member);
+            return memberMapper.memberToMemberDTO(savedMember);
+        }catch(Exception e){
+            throw new CustomException(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
