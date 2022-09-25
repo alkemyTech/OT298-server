e1bb5337364fa89bab0b78d0ab1d96543fc415d4
Alejandro <alj70111@gmail.com>
2022-09-25 00:47:17 -0300

    add static API KEY constant

--- src/main/java/com/alkemy/ong/service/Impl/EmailServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/Impl/EmailServiceImpl.java
@@ -3,22 +3,14 @@ package com.alkemy.ong.service.Impl;
 import com.alkemy.ong.service.IEmailService;
 import com.sendgrid.helpers.mail.objects.Content;
 import com.sendgrid.helpers.mail.objects.Email;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
-
 
 public class EmailServiceImpl implements IEmailService {
-
-    @Autowired
-    private Environment env;
     @Value("${ong.email.sender}")
     private String emailSender;
-
+    private static final String API_KEY = "EMAIL_API_KEY";
     @Override
     public void sendWelcomeEmailTo(String to) {
-
-        String apiKey = env.getProperty("EMAIL_API_KEY");
 
         Email fromEmail = new Email(emailSender);
         Email toEmail = new Email(to);
