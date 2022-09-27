30434ba7287195c12c1c1b26411ff91011a76fe1
Andres Hoyos Garcia <andyholesdev@gmail.com>
2022-09-27 16:49:01 -0300

    Update ActivityDataLoader.java

--- src/main/java/com/alkemy/ong/util/ActivityDataLoader.java
+++ src/main/java/com/alkemy/ong/util/ActivityDataLoader.java
@@ -18,7 +18,7 @@ public class ActivityDataLoader implements CommandLineRunner {
     }
 
     private void loadActivityData(){
-        if (!activityService.getAll().isEmpty()) {
+        if (activityService.getAll().isEmpty()) {
             ActivityDTO activity1 = new ActivityDTO();
             activity1.setName("Apoyo Escolar para el nivel Primario");
             activity1.setContent("El espacio de apoyo escolar es el corazón del área educativa. Se realizan los talleres de lunes a jueves de 10 a 12 horas y de 14 a 16 horas en el contraturno, Los sábados también se realiza el taller para niños y niñas que asisten a la escuela doble turno. Tenemos un espacio especial para los de 1er grado 2 veces por semana ya que ellos necesitan atención especial! Actualmente se encuentran inscriptos a este programa 150 niños y niñas de 6 a 15 años. Este taller está pensado para ayudar a los alumnos con el material que traen de la escuela, también tenemos una docente que les da clases de lengua y matemática con una planificación propia que armamos en Manos para nivelar a los niños y que vayan con más herramientas a la escuela.");
