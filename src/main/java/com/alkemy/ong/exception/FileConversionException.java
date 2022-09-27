3d3981fd6f7374b8c50f16316dbbfbf416be090e
joeldevel <christianjoel.developer@gmail.com>
2022-09-26 21:21:33 -0300

    adds exception for file conversion error

---
+++ src/main/java/com/alkemy/ong/exception/FileConversionException.java
@@ -0,0 +1,7 @@
+package com.alkemy.ong.exception;
+
+public class FileConversionException extends RuntimeException {
+    public FileConversionException(String error) {
+        super(error);
+    }
+}
