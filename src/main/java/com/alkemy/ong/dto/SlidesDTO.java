b09d31978a0706af27bfe3614eeef1f4a262e83a
Alejandro <alj70111@gmail.com>
2022-09-28 18:38:20 -0300

    update develop

---
+++ src/main/java/com/alkemy/ong/dto/SlidesDTO.java
@@ -0,0 +1,18 @@
+package com.alkemy.ong.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SlidesDTO {
+
+    private Long id;
+
+    private String image;
+
+    private String text;
+
+    private int orders;
+
+}
---
+++ src/main/java/com/alkemy/ong/service/ISlidesService.java
@@ -0,0 +1,9 @@
+package com.alkemy.ong.service;
+
+import com.alkemy.ong.dto.SlidesDTO;
+
+public interface ISlidesService {
+
+    SlidesDTO save(SlidesDTO slidesDTO);
+
+}
