630fb3366e6c2b99017326734e8a196a9eaae50d
CarolinaCamilaQuevedo <CarolinaCQ>
2022-10-03 14:00:33 -0300

    OT29845 fix: delete import

--- src/main/java/com/alkemy/ong/controller/NewsController.java
+++ src/main/java/com/alkemy/ong/controller/NewsController.java
@@ -3,12 +3,10 @@ package com.alkemy.ong.controller;
 import com.alkemy.ong.dto.NewsDto;
 import com.alkemy.ong.service.INewsService;
 
-import javax.annotation.security.RolesAllowed;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
