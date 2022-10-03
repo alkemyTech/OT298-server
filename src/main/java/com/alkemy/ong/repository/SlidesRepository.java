b652baf215e7884359f80ffc3ef27fcc28214166
laudebor <laudebor@gmail.com>
2022-10-03 15:45:02 -0300

    Slides added to public data endpoint from Organization

--- src/main/java/com/alkemy/ong/repository/SlidesRepository.java
+++ src/main/java/com/alkemy/ong/repository/SlidesRepository.java
@@ -1,10 +1,18 @@
 package com.alkemy.ong.repository;
 
 import com.alkemy.ong.model.Slides;
+import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
 
 @Repository
 public interface SlidesRepository extends JpaRepository<Slides, Long> {
 
+    @Query("SELECT s FROM Slides s WHERE s.organizationId = :orgId ORDER BY s.orders")
+    List<Slides> findAllByOrganization(@Param("orgId") Long organizationId);
 }
--- src/main/java/com/alkemy/ong/service/impl/OrganizationServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/impl/OrganizationServiceImpl.java
@@ -1,12 +1,18 @@
 package com.alkemy.ong.service.impl;
 
 import com.alkemy.ong.dto.OrganizationBasicDTO;
+import com.alkemy.ong.dto.SlidesDTO;
 import com.alkemy.ong.mapper.OrganizationMapper;
+import com.alkemy.ong.mapper.SlidesMapper;
 import com.alkemy.ong.model.Organization;
+import com.alkemy.ong.model.Slides;
 import com.alkemy.ong.repository.OrganizationRepository;
+import com.alkemy.ong.repository.SlidesRepository;
 import com.alkemy.ong.service.IOrganizationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class OrganizationServiceImpl implements IOrganizationService {
@@ -17,9 +23,20 @@ public class OrganizationServiceImpl implements IOrganizationService {
     @Autowired
     OrganizationRepository organizationRepository;
 
+    @Autowired
+    SlidesRepository slidesRepository;
+
+    @Autowired
+    SlidesMapper slidesMapper;
+
     @Override
     public OrganizationBasicDTO getOrganizationBasic() {
         Organization organization = organizationRepository.findFirstByOrderByCreationDateDesc();
-        return organizationMapper.organizationToOrganizationBasicDTO(organization);
+        List<SlidesDTO> slides = slidesMapper
+                .listSlidesToDtos(slidesRepository.findAllByOrganization(organization.getId()));
+        OrganizationBasicDTO organizationBasicDTO = organizationMapper
+                .organizationToOrganizationBasicDTO(organization);
+        organizationBasicDTO.setSlides(slides);
+        return organizationBasicDTO;
     }
 }
