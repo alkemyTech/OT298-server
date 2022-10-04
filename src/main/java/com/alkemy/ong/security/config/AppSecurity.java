71d96719e473afa5b20420799bd112579b817031
andyholes <andyholesdev@gmail.com>
2022-10-04 19:19:49 -0300

    Add get contacts endpoint

--- src/main/java/com/alkemy/ong/security/config/AppSecurity.java
+++ src/main/java/com/alkemy/ong/security/config/AppSecurity.java
@@ -3,7 +3,6 @@ package com.alkemy.ong.security.config;
 import com.alkemy.ong.security.filter.JwtRequestFilter;
 import com.alkemy.ong.security.service.impl.UserServiceImpl;
 import static com.alkemy.ong.util.Constants.ALL_ROLES;
-import static com.alkemy.ong.util.Constants.Endpoints.SLIDE_UPDATE;
 import static com.alkemy.ong.util.Constants.ROLE_ADMIN;
 import static com.alkemy.ong.util.Constants.Endpoints.USER_UPDATE;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,7 +42,6 @@ public class AppSecurity extends WebSecurityConfigurerAdapter {
                 .antMatchers("/auth/*").permitAll()
                 .antMatchers(HttpMethod.GET, "/**").hasAnyAuthority(ROLE_ADMIN)
                 .antMatchers(HttpMethod.PATCH, USER_UPDATE).hasAnyAuthority(ALL_ROLES)
-//                .antMatchers("/media/upload**").hasAuthority("ROLE_ADMIN")/*To be defined*/
                 .antMatchers(HttpMethod.PUT, "/news/{id}").hasAnyAuthority(ROLE_ADMIN)
                 .antMatchers(HttpMethod.DELETE, "/news/{id}").hasAnyAuthority(ROLE_ADMIN)
                 .antMatchers(HttpMethod.GET, "/news/{id}").hasAnyAuthority(ROLE_ADMIN)
@@ -51,6 +49,7 @@ public class AppSecurity extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.PUT, "/categories/{id}").hasAnyAuthority(ROLE_ADMIN)
                 .antMatchers(HttpMethod.GET, "/categories/{id}").hasAnyAuthority(ROLE_ADMIN)
                 .antMatchers(HttpMethod.GET, "/organization/public").hasAnyAuthority(ALL_ROLES)
+                .antMatchers(HttpMethod.GET, "/contacts").hasAnyAuthority(ROLE_ADMIN)
                 .anyRequest().authenticated()
                 .and().exceptionHandling()
                 .and().sessionManagement()
