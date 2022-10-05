cb4a6d5b9e5c0c37c6f538d55e5e38d41862558f
joeldevel <christianjoel.developer@gmail.com>
2022-10-05 18:54:21 -0300

    adds endpoint for retrieving a slide by id

--- src/main/java/com/alkemy/ong/controller/SlidesController.java
+++ src/main/java/com/alkemy/ong/controller/SlidesController.java
@@ -3,13 +3,10 @@ package com.alkemy.ong.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
+
 import static com.alkemy.ong.util.Constants.Endpoints;
+
 import com.alkemy.ong.service.ISlidesService;
 import com.alkemy.ong.exception.ResourceNotFoundException;
 import com.alkemy.ong.dto.SlidesDTO;
@@ -19,19 +16,25 @@ import com.alkemy.ong.dto.SlidesDTO;
 public class SlidesController {
     @Autowired
     private ISlidesService slidesService;
-    
+
     @PostMapping
-    public ResponseEntity<SlidesDTO> save(@RequestBody SlidesDTO slidesDTO){
+    public ResponseEntity<SlidesDTO> save(@RequestBody SlidesDTO slidesDTO) {
 
         SlidesDTO slidesSave = slidesService.save(slidesDTO);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(slidesSave);
 
     }
-    
+
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> delete(@PathVariable Long id) throws ResourceNotFoundException{
+    public ResponseEntity<?> delete(@PathVariable Long id) throws ResourceNotFoundException {
         return new ResponseEntity<>(slidesService.delete(id), HttpStatus.ACCEPTED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<SlidesDTO> getSlidesById(@PathVariable Long id) {
+        SlidesDTO dto = slidesService.getById(id);
+        return ResponseEntity.ok(dto);
     }
 }
 
