1f95c5c124d77243881f7d8639d5926cea3d2672
joeldevel <christianjoel.developer@gmail.com>
2022-10-09 16:41:56 -0300

    adds method for pagination

--- src/main/java/com/alkemy/ong/repository/NewsRepository.java
+++ src/main/java/com/alkemy/ong/repository/NewsRepository.java
@@ -1,7 +1,10 @@
 package com.alkemy.ong.repository;
 
 import com.alkemy.ong.model.News;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +13,5 @@ import java.util.Optional;
 @Repository
 public interface NewsRepository extends JpaRepository<News, Long> {
     boolean existsById(Long id);
+    Page<News> findAll(Pageable pageable);
 }
