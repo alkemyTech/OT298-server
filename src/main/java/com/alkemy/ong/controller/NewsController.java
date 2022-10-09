bc76a22c71bffbca4326a4d7558dfb05441131dc
joeldevel <christianjoel.developer@gmail.com>
2022-10-09 16:37:42 -0300

    adds paginated news endpoint

--- src/main/java/com/alkemy/ong/controller/NewsController.java
+++ src/main/java/com/alkemy/ong/controller/NewsController.java
@@ -1,13 +1,22 @@
 package com.alkemy.ong.controller;
 
 import com.alkemy.ong.dto.NewsDto;
+import com.alkemy.ong.dto.NewsPaginationDto;
+import com.alkemy.ong.model.News;
+import com.alkemy.ong.repository.NewsRepository;
 import com.alkemy.ong.service.INewsService;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+
+import com.alkemy.ong.util.Pagination;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/news")
@@ -16,26 +25,40 @@ public class NewsController {
     @Autowired
     private INewsService newsService;
 
+    @Autowired
+    private NewsRepository newsRepository;
+    
     @PostMapping
-    public ResponseEntity<?> saveNews(@Valid @RequestBody NewsDto newsDto) throws Exception{
+    public ResponseEntity<?> saveNews(@Valid @RequestBody NewsDto newsDto) throws Exception {
         return new ResponseEntity<>(newsService.save(newsDto), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<NewsDto> getById(@PathVariable Long id){
+    public ResponseEntity<NewsDto> getById(@PathVariable Long id) {
         NewsDto newsDto = newsService.getById(id);
         return ResponseEntity.ok().body(newsDto);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<NewsDto> updateNews(@PathVariable Long id, @Valid @RequestBody NewsDto newsDto){
+    public ResponseEntity<NewsDto> updateNews(@PathVariable Long id, @Valid @RequestBody NewsDto newsDto) {
         NewsDto news = newsService.update(id, newsDto);
         return new ResponseEntity<NewsDto>(news, HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteNews(@PathVariable Long id){
+    public ResponseEntity<Void> deleteNews(@PathVariable Long id) {
         newsService.deleteById(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
+
+
+    @GetMapping()
+    public ResponseEntity<NewsPaginationDto> getPaginated(
+            @Valid @PageableDefault(page = Pagination.INITIAL_PAGE, size = Pagination.PAGE_SIZE) Pageable pageable,
+            HttpServletRequest request,
+            UriComponentsBuilder uriBuilder) {
+        return ResponseEntity.ok(newsService.getPaginated(pageable, request, uriBuilder));
+    }
+
+
 }
