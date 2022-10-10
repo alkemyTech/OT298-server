f3c84b114d8a0dfe5c98b2cae4ee898b140d3bb1
Laura de Borbón <52927859+laudebor@users.noreply.github.com>
2022-10-10 11:57:37 -0300

    Added pagination to categories (#68)

--- src/main/java/com/alkemy/ong/util/Constants.java
+++ src/main/java/com/alkemy/ong/util/Constants.java
@@ -4,6 +4,10 @@ public class Constants {
     public static final String ROLE_ADMIN = "ROLE_ADMIN";
     public static final String ROLE_USER = "ROLE_USER";
     public static final String[] ALL_ROLES = {ROLE_ADMIN, ROLE_USER};
+
+    public static final Integer PAGE_SIZE = 10;
+
+    public static final String FIRST_PAGE = "0";
     public static abstract class Endpoints {
         public static final String USER = "/users";
         public static final String USER_UPDATE = USER + "/{id}";
