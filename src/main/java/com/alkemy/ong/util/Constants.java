427b8e24c5e5e60d1878a63fddcce5c0a4e66cc5
andyholes <andyholesdev@gmail.com>
2022-10-12 17:37:21 -0300

    Endpoint constant changes

--- src/main/java/com/alkemy/ong/util/Constants.java
+++ src/main/java/com/alkemy/ong/util/Constants.java
@@ -14,17 +14,30 @@ public class Constants {
     public static final Integer FIRST_PAGE_INTEGER = 0;
 
     public static abstract class Endpoints {
+        public static final String ID = "/{id}";
+        public static final String PAGE = "/page";
         public static final String USER = "/users";
-        public static final String USER_UPDATE = USER + "/{id}";
+        public static final String USER_ID = USER + ID;
         public static final String SLIDE = "/slides";
-        public static final String SLIDE_UPDATE = SLIDE + "{/id}";
+        public static final String SLIDE_ID = SLIDE + ID;
+        public static final String CONTACT = "/contacts";
+        public static final String ORGANIZATION = "/organization/public";
+        public static final String ORGANIZATION_ID = ORGANIZATION + ID;
+        public static final String COMMENT = "/comments";
+        public static final String NEWS = "/news";
+        public static final String NEWS_ID = NEWS + ID;
+        public static final String NEWS_ID_COMMENT = NEWS + ID + COMMENT;
+        public static final String ACTIVITY = "/activities";
+        public static final String CATEGORY = "/categories";
+        public static final String CATEGORY_ID = CATEGORY + ID;
         public static final String MEMBER = "/members";
-        public static final String MEMBER_UPDATE = MEMBER + "/{id}";
+        public static final String MEMBER_ID = MEMBER + ID;
+        public static final String MEMBER_PAGE = MEMBER + PAGE;
         public static final String API_UI_ANTMATCHER = "/api/**";
         public static final String API_DESCRIPTION_ANTMATCHER = "/v3/**";
-
         public static final String TESTIMONIAL ="/testimonials";
-        public static final String TESTIMONIAL_ID = "/{id}";
-        public static final String TESTIMONIAL_PAGE = "/page";
+        public static final String TESTIMONIAL_ID = TESTIMONIAL + ID;
+        public static final String TESTIMONIAL_PAGE = TESTIMONIAL + PAGE;
     }
 }
+
