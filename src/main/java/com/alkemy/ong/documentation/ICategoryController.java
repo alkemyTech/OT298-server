f28c9789dce35723fcdfe2d6a6987ea9634ef00f
Laura de Borbón <52927859+laudebor@users.noreply.github.com>
2022-10-13 13:11:17 -0300

    Feature OT298-86: Added documentation to categories (#77)
    * Added security configuration to OpenAPIConfig
    
    * Changed HTTP Status from No Categories Exception Handler
    
    * Added docs messages and codes constants
    
    * Added docs package
    
    * Added documentation to categories
    
    * Deleted comments and unused imports
    
    * Changed documentation package
    
    * Added category documentation to getCategoryById method
    
    * Added category documentation to save category endpoint
    
    * Added category documentation to delete and update endpoints
    
    * Changed category controller interface name
    
    * REST method annotations moved to CategoryController

---
+++ src/main/java/com/alkemy/ong/documentation/ICategoryController.java
@@ -0,0 +1,74 @@
+package com.alkemy.ong.documentation;
+
+import com.alkemy.ong.dto.CategoryCompleteGetDto;
+import com.alkemy.ong.dto.CategoryDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Map;
+
+import static com.alkemy.ong.util.Constants.FIRST_PAGE;
+import static com.alkemy.ong.util.Constants.httpCodes.*;
+import static com.alkemy.ong.util.Constants.messagesForDocs.*;
+
+@Tag(name = "Categories", description = "View, add, update and delete categories")
+public interface ICategoryController {
+
+    @Operation(summary = GET_CATEGORIES)
+    @SecurityRequirement(name = BEARER_AUTH)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = STATUS_OK, description = GET_CATEGORIES_SUCCESSFUL, content = @Content),
+            @ApiResponse(responseCode = STATUS_BAD_REQUEST, description = INVALID_PAGE, content = @Content),
+            @ApiResponse(responseCode = STATUS_NOT_FOUND, description = PAGE_NOT_FOUND, content = @Content),
+            @ApiResponse(responseCode = STATUS_NO_CONTENT, description = NO_CATEGORIES, content = @Content),
+            @ApiResponse(responseCode = STATUS_FORBIDDEN, description = FORBIDDEN, content = @Content)
+    })
+    ResponseEntity<Map<String, Object>> getAllCategories(
+            @RequestParam(defaultValue = FIRST_PAGE, required = false) Integer page);
+
+    @Operation(summary = GET_CATEGORY_ID)
+    @SecurityRequirement(name = BEARER_AUTH)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = STATUS_OK, description = GET_CATEGORY_SUCCESSFUL, content = @Content),
+            @ApiResponse(responseCode = STATUS_NOT_FOUND, description = CATEGORY_NOT_FOUND, content = @Content),
+            @ApiResponse(responseCode = STATUS_FORBIDDEN, description = FORBIDDEN, content = @Content)
+    })
+    ResponseEntity<CategoryCompleteGetDto> getCategoryById(@PathVariable Long id);
+
+
+    @Operation(summary = ADD_CATEGORY)
+    @SecurityRequirement(name = BEARER_AUTH)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = STATUS_CREATED, description = CATEGORY_CREATED, content = @Content),
+            @ApiResponse(responseCode = STATUS_BAD_REQUEST, description = BAD_REQUEST, content = @Content),
+            @ApiResponse(responseCode = STATUS_FORBIDDEN, description = FORBIDDEN, content = @Content)
+    })
+    ResponseEntity<CategoryDTO> saveCategory (@Valid @RequestBody CategoryDTO dto);
+
+    @Operation(summary = DELETE_CATEGORY)
+    @SecurityRequirement(name = BEARER_AUTH)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = STATUS_NO_CONTENT, description = CATEGORY_DELETED, content = @Content),
+            @ApiResponse(responseCode = STATUS_NOT_FOUND, description = CATEGORY_NOT_FOUND, content = @Content),
+            @ApiResponse(responseCode = STATUS_FORBIDDEN, description = FORBIDDEN, content = @Content)
+    })
+    ResponseEntity<Void> deleteCategory(@PathVariable Long id);
+
+    @Operation(summary = UPDATE_CATEGORY)
+    @SecurityRequirement(name = BEARER_AUTH)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = STATUS_OK, description = CATEGORY_UPDATED, content = @Content),
+            @ApiResponse(responseCode = STATUS_NOT_FOUND, description = CATEGORY_NOT_FOUND, content = @Content),
+            @ApiResponse(responseCode = STATUS_BAD_REQUEST, description = BAD_REQUEST, content = @Content),
+            @ApiResponse(responseCode = STATUS_FORBIDDEN, description = FORBIDDEN, content = @Content)
+    })
+    ResponseEntity<CategoryDTO> updateCategory(@PathVariable Long id,
+                                                      @Valid @RequestBody CategoryDTO dto);
+    }
