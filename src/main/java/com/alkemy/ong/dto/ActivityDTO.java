c49e385849aea378356e80305d7e0e8d7162a5e8
joeldevel <christianjoel.developer@gmail.com>
2022-10-16 21:51:05 -0300

    adds annotations for testing

--- src/main/java/com/alkemy/ong/dto/ActivityDTO.java
+++ src/main/java/com/alkemy/ong/dto/ActivityDTO.java
@@ -1,6 +1,8 @@
 package com.alkemy.ong.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Lob;
@@ -8,6 +10,8 @@ import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class ActivityDTO {
 
     private Long id;
