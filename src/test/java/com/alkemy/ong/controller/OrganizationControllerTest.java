6759af8691e2ddcde2da3fc94083d72dbbc63541
laudebor <laudebor@gmail.com>
2022-10-18 21:50:09 -0300

    Added setUp to Organization controller test

--- src/test/java/com/alkemy/ong/controller/OrganizationControllerTest.java
+++ src/test/java/com/alkemy/ong/controller/OrganizationControllerTest.java
@@ -1,5 +1,8 @@
 package com.alkemy.ong.controller;
 
+import com.alkemy.ong.dto.OrganizationBasicDTO;
+import com.alkemy.ong.dto.OrganizationUpdateDTO;
+import com.alkemy.ong.dto.SlidesDTO;
 import com.alkemy.ong.security.service.IUserService;
 import com.alkemy.ong.security.service.impl.UserServiceImpl;
 import com.alkemy.ong.service.IOrganizationService;
@@ -13,10 +16,12 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static com.alkemy.ong.util.Constants.Endpoints.ORGANIZATION;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -32,9 +37,36 @@ class OrganizationControllerTest {
     @Mock
     private IOrganizationService organizationService;
 
+    private OrganizationUpdateDTO organizationUpdateDTO;
+
+    private OrganizationBasicDTO organizationBasicDTO;
+
+    private List<SlidesDTO> slidesDTOList;
+
     @BeforeEach
     void setUp() {
+        organizationUpdateDTO = new OrganizationUpdateDTO("SomosMas",
+                12345678, "address",
+                "ong.png",
+                "welcome",
+                "ong",
+                "facebook.com/somosmas",
+                "linkedin.com/somosmas",
+                "instagram.com/somosmas",
+                "somosmas@ong.com"
+                );
+        slidesDTOList = new ArrayList<>();
+        slidesDTOList.add(new SlidesDTO(1L, "slide1.jpg", "text1", 1));
+        organizationBasicDTO = new OrganizationBasicDTO("SomosMas",
+                12345678,
+                "address",
+                "ong.png",
+                slidesDTOList,
+                "facebook.com/somosmas",
+                "linkedin.com/somosmas",
+                "instagram.com/somosmas");
     }
+
 
     @Test
     void getOrganizationForbidden() throws Exception {
