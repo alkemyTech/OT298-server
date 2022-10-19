aa58a688a3962449d31079a25dae1ef942708cea
laudebor <laudebor@gmail.com>
2022-10-19 19:52:43 -0300

    Added get organization test and display names

--- src/test/java/com/alkemy/ong/controller/OrganizationControllerTest.java
+++ src/test/java/com/alkemy/ong/controller/OrganizationControllerTest.java
@@ -8,13 +8,13 @@ import com.alkemy.ong.security.service.impl.UserServiceImpl;
 import com.alkemy.ong.service.IOrganizationService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.MessageSource;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
@@ -22,8 +22,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.alkemy.ong.util.Constants.*;
 import static com.alkemy.ong.util.Constants.Endpoints.ORGANIZATION;
-import static com.alkemy.ong.util.Constants.ROLE_ADMIN;
 import static org.mockito.Mockito.when;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
@@ -74,11 +74,25 @@ class OrganizationControllerTest {
                 "address",
                 "ong.png",
                 slidesDTOList,
-                "facebook.com/somosmas",
-                "linkedin.com/somosmas",
-                "instagram.com/somosmas");
+                "https://www.facebook.com/somosmas",
+                "https://www.linkedin.com/profile/somosmas",
+                "https://www.instagram.com/somosmas");
+    }
+
+    @Test
+    @DisplayName("All users can get public information of the organization")
+    @WithMockUser(username = "user", authorities = {ROLE_ADMIN, ROLE_USER})
+    void getOrganizationWithAuthorities() throws Exception{
+        when(organizationService.getOrganizationBasic())
+                .thenReturn(organizationBasicDTO);
+        mockMvc.perform(get("/organization/public")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .with(user("user").roles("ADMIN", "USER"))
+                        .with(csrf()))
+                .andExpect(status().isOk());
     }
     @Test
+    @DisplayName("Admin users can update the organization data")
     @WithMockUser(username = "admin", authorities = ROLE_ADMIN)
     void updateOrganizationByAdmin() throws Exception {
         when(organizationService.updateOrganization(1L, organizationUpdateDTO)).
@@ -92,6 +106,7 @@ class OrganizationControllerTest {
     }
 
     @Test
+    @DisplayName("Cannot update a non existing organization")
     @WithMockUser(username = "admin", authorities = ROLE_ADMIN)
     void updateNonExistingOrganizationFailed() throws Exception{
         Long idOrgNotExist = 3L;
@@ -106,6 +121,7 @@ class OrganizationControllerTest {
     }
 
     @Test
+    @DisplayName("Update of the organization data fails because the email format is wrong")
     @WithMockUser(username = "admin", authorities = ROLE_ADMIN)
     void updateOrganizationWrongEmailFormat() throws Exception {
         organizationUpdateDTO.setEmail("email");
@@ -120,6 +136,7 @@ class OrganizationControllerTest {
     }
 
     @Test
+    @DisplayName("Update of the organization data fails because the Facebook url format is wrong")
     @WithMockUser(username = "admin", authorities = ROLE_ADMIN)
     void updateOrganizationWrongFacebookUrlFormat() throws Exception {
         organizationUpdateDTO.setUrlFacebook("facebook");
@@ -134,6 +151,7 @@ class OrganizationControllerTest {
     }
 
     @Test
+    @DisplayName("Update of the organization data fails because the Instagram url format is wrong")
     @WithMockUser(username = "admin", authorities = ROLE_ADMIN)
     void updateOrganizationWrongInstagramUrlFormat() throws Exception {
         organizationUpdateDTO.setUrlInstagram("instagram");
@@ -148,6 +166,7 @@ class OrganizationControllerTest {
     }
 
     @Test
+    @DisplayName("Update of the organization data fails because the Linkedin url format is wrong")
     @WithMockUser(username = "admin", authorities = ROLE_ADMIN)
     void updateOrganizationWrongLinkedinUrlFormat() throws Exception {
         organizationUpdateDTO.setUrlLinkedin("linkedin");
@@ -160,8 +179,8 @@ class OrganizationControllerTest {
                         .with(csrf()))
                 .andExpect(status().isBadRequest());
     }
-
     @Test
+    @DisplayName("Cannot update the organization data with no credentials")
     void updateOrganizationForbidden() throws Exception {
         mockMvc.perform(patch(ORGANIZATION + 1L)
                         .contentType(MediaType.APPLICATION_JSON_VALUE)
@@ -169,8 +188,8 @@ class OrganizationControllerTest {
                 .andExpect(status().isForbidden());
     }
     @Test
+    @DisplayName("Cannot update the organization data with no credentials")
     void getOrganizationForbidden() throws Exception {
-
         mockMvc.perform(get(ORGANIZATION)
                     .contentType(MediaType.APPLICATION_JSON_VALUE)
                     .with(csrf()))
