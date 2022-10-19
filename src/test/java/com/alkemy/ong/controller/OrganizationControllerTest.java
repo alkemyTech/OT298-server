d6f0aa93702b839f72157b6d5e0750416888bd24
laudebor <laudebor@gmail.com>
2022-10-19 18:41:45 -0300

    Added update non existing organization test

--- src/test/java/com/alkemy/ong/controller/OrganizationControllerTest.java
+++ src/test/java/com/alkemy/ong/controller/OrganizationControllerTest.java
@@ -3,6 +3,7 @@ package com.alkemy.ong.controller;
 import com.alkemy.ong.dto.OrganizationBasicDTO;
 import com.alkemy.ong.dto.OrganizationUpdateDTO;
 import com.alkemy.ong.dto.SlidesDTO;
+import com.alkemy.ong.exception.ResourceNotFoundException;
 import com.alkemy.ong.security.service.impl.UserServiceImpl;
 import com.alkemy.ong.service.IOrganizationService;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -13,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.MessageSource;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
@@ -87,6 +89,20 @@ class OrganizationControllerTest {
                         .with(user("admin").roles("ADMIN"))
                         .with(csrf()))
                 .andExpect(status().isAccepted());
+    }
+
+    @Test
+    @WithMockUser(username = "admin", authorities = ROLE_ADMIN)
+    void updateNonExistingOrganizationFailed() throws Exception{
+        Long idOrgNotExist = 3L;
+        when(organizationService.updateOrganization(idOrgNotExist, organizationUpdateDTO)).
+                thenThrow(ResourceNotFoundException.class);
+        mockMvc.perform(patch("/organization/public/" + idOrgNotExist)
+                        .contentType(APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(organizationUpdateDTO))
+                        .with(user("admin").roles("ADMIN"))
+                        .with(csrf()))
+                .andExpect(status().isNotFound());
     }
 
     @Test
