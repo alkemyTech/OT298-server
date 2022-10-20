70d5b525e8a461ee4c27f4169405f1688a1e6e0c
Alejandro <alj70111@gmail.com>
2022-10-20 14:51:07 -0300

    add test category

--- src/main/java/com/alkemy/ong/dto/CategoryDTO.java
+++ src/main/java/com/alkemy/ong/dto/CategoryDTO.java
@@ -1,6 +1,8 @@
 package com.alkemy.ong.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.validation.constraints.NotNull;
@@ -8,6 +10,8 @@ import javax.validation.constraints.Pattern;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class CategoryDTO {
 
     private Long id;
